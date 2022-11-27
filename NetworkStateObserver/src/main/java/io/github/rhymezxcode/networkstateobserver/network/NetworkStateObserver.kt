7fa4a5f9ad54e93906069b571240938fcc826f2d
Awodire babajide samuel <rhymezx.code@gmail.com>
2022-11-27 08:44:42 +0100

    Added a builder for the NetworkStateObserver

--- NetworkStateObserver/src/main/java/io/github/rhymezxcode/networkstateobserver/network/NetworkStateObserver.kt
+++ NetworkStateObserver/src/main/java/io/github/rhymezxcode/networkstateobserver/network/NetworkStateObserver.kt
@@ -3,25 +3,29 @@ package io.github.rhymezxcode.networkstateobserver.network
 import android.app.Activity
 import androidx.lifecycle.LifecycleOwner
 
-class NetworkStateObserver(private val viewLifecycleOwner: LifecycleOwner,
-                           private val activity: Activity) {
+class NetworkStateObserver(
+    private val viewLifecycleOwner: LifecycleOwner?,
+    private val activity: Activity?
+) {
     private lateinit var checkNetworkConnection: CheckNetworkConnection
     private var reach = MyReachability("www.google.com", "www.github.com")
-    private var context = activity.applicationContext
+    private var context = activity?.applicationContext
     private fun callNetworkConnection() {
-        checkNetworkConnection = CheckNetworkConnection(activity.application)
-        checkNetworkConnection.observe(viewLifecycleOwner) { isConnected ->
+        checkNetworkConnection = CheckNetworkConnection(activity?.application!!)
+        checkNetworkConnection.observe(viewLifecycleOwner!!) { isConnected ->
             if (isConnected) {
                 val loader = Thread {
                     when {
-                        reach.hasServerConnected(context) ->
+                        reach.hasServerConnected(context!!) ->
                             activity.runOnUiThread {
 
                             }
-                        reach.hasInternetConnected(context) ->
+
+                        reach.hasInternetConnected(context!!) ->
                             activity.runOnUiThread {
                                 //check for weak connection
                             }
+
                         else -> activity.runOnUiThread {
                             //check for lost connection
                         }
@@ -35,4 +39,22 @@ class NetworkStateObserver(private val viewLifecycleOwner: LifecycleOwner,
         }
 
     }
+
+    private constructor(builder: Builder) : this(builder.viewLifecycleOwner, builder.activity)
+
+    class Builder {
+        var viewLifecycleOwner: LifecycleOwner? = null
+            private set
+
+        var activity: Activity? = null
+            private set
+
+        fun lifecycleOwner(viewLifecycleOwner: LifecycleOwner) =
+            apply { this.viewLifecycleOwner = viewLifecycleOwner}
+
+        fun activity(activity: Activity) = apply { this.activity = activity }
+
+        fun build() = NetworkStateObserver(this)
+    }
+
 }
